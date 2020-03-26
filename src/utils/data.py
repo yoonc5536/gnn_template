a29ee301389206d9a0feb4eb321f4cf42b76e3d2
Junyoung Park <junyoungpark@kaist.ac.kr>
2020-03-26 13:10:59 +0900

    add graph-tensor data set and loader

--- src/utils/data.py
+++ src/utils/data.py
@@ -41,3 +41,43 @@ class DGLGraphDataLoader(DataLoader):
         x.to(torch.device(self.device))
         y.to(torch.device(self.device))
         return [x, y]
+
+
+class DGLGraphTensorDataset(Dataset):
+
+    def __init__(self, graphs, target):
+        assert isinstance(graphs, list), "expected spec of 'x_graphs' is a list of graphs"
+
+        assert len(graphs) == target.shape[0]
+
+        self.x_data = graphs
+        self.y_data = target
+        self.len = len(graphs)
+
+    def __getitem__(self, idx):
+        return self.x_data[idx], self.y_data[idx]
+
+    def __len__(self):
+        return self.len
+
+
+class DGLGraphTensorDataLoader(DataLoader):
+
+    def __init__(self, *args, **kwargs):
+        if 'device' in kwargs:
+            device = kwargs.pop('device')
+        else:
+            device = 'cpu'
+
+        self.device = device
+        collate_fn = self.collate_fn
+        kwargs['collate_fn'] = collate_fn
+        super(DGLGraphTensorDataLoader, self).__init__(*args, **kwargs)
+
+    def collate_fn(self, batch):
+        x = dgl.batch([item[0] for item in batch])
+        y = torch.cat([item[1] for item in batch], dim=0)
+
+        x.to(torch.device(self.device))
+        y = y.to(self.device)
+        return [x, y]
